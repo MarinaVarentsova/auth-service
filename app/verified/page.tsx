7b2e1a41a64059aa2009bccc23d56652e1c8b976
MarinaVarentsova <87182320+MarinaVarentsova@users.noreply.github.com>
2026-07-04 18:08:03 +0300

    Create page.tsx

---
+++ app/verified/page.tsx
@@ -0,0 +1,8 @@
+export default function VerifiedPage() {
+  return (
+    <main style={{padding:40}}>
+      <h1>Email подтвержден</h1>
+      <p>Теперь можно войти в приложение.</p>
+    </main>
+  );
+}
