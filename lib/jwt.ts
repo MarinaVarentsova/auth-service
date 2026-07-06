7b22d42efbcb32e310c035f67109ad70d8b4b4a9
MarinaVarentsova <87182320+MarinaVarentsova@users.noreply.github.com>
2026-07-06 01:38:51 -0700

    Update jwt.ts

--- lib/jwt.ts
+++ lib/jwt.ts
@@ -1,13 +1,17 @@
 import jwt from "jsonwebtoken";
 
-const SECRET = process.env.AUTH_SECRET!;
+const SECRET = process.env.AUTH_SECRET || "dev-secret";
 
 export function createAccessToken(payload: any) {
-  return jwt.sign(payload, SECRET, {
-    expiresIn: process.env.JWT_EXPIRES || "15m",
-  });
+  return jwt.sign(
+    payload,
+    SECRET as any,
+    {
+      expiresIn: (process.env.JWT_EXPIRES || "15m") as any,
+    } as any
+  );
 }
 
 export function verifyAccessToken(token: string) {
-  return jwt.verify(token, SECRET);
+  return jwt.verify(token, SECRET as any);
 }
